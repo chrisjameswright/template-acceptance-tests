e2110b3605a68793bd5e137fb84fe4ccca3f0da8
Paul Anderson <paul.anderson@digital.hmrc.gov.uk>
2018-02-19 09:45:30 +0000

    cucumberOptionFormatFix replaced deprecated Format with Plugin

--- src/test/scala/uk/gov/hmrc/integration/cucumber/utils/RunnerQA.scala
+++ src/test/scala/uk/gov/hmrc/integration/cucumber/utils/RunnerQA.scala
@@ -8,7 +8,7 @@ import org.junit.runner.RunWith
 @CucumberOptions(
   features = Array("src/test/resources/features"),
   glue = Array("uk.gov.hmrc.integration.cucumber.stepdefs"),
-  format = Array ("pretty", "html:target/cucumber", "json:target/cucumber.json"),
+  plugin = Array ("pretty", "html:target/cucumber", "json:target/cucumber.json"),
   tags = Array("@Smoke, @Shutdown")
 )
 class RunnerQA {
