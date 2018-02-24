9a62aac5bdb661739da15c13d500cb52804152bd
Chris Poole <christopher.poole@digital.hmrc.gov.uk>
2018-02-24 11:04:30 +0000

    Removed Drivers and replaced with installation scripts

--- run_local.sh
+++ run_local.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 ENV="local"
 BROWSER="firefox"
+DRIVER_PATH=/usr/local/bin/geckodriver
 
-sbt -Dbrowser=$BROWSER -Denvironment=$ENV  -Dwebdriver.gecko.driver=/drivers/geckodriver 'test-only uk.gov.hmrc.integration.cucumber.utils.Runner'
+sbt -Dbrowser=$BROWSER -Denvironment=$ENV -Dwebdriver.gecko.driver=${DRIVER_PATH} 'test-only uk.gov.hmrc.integration.cucumber.utils.Runner'
